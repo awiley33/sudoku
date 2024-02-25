8e4b661cb7b5896165906566a6189a4b9252598d
Anna Wiley <awiley33@gmail.com>
2024-02-25 13:12:29 -0600

    Update sector validation method

--- sudoku.rb
+++ sudoku.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
 
   def initialize(difficulty = :easy)
@@ -52,36 +54,44 @@ class Board
   end
 
   def create_sectors
+    # initialize empty arrays to use for data manipulation
     # for row arrays 0-2
-      # shovels elements 0-2 into sector array 0
-      # shovels elements 3-5 into sector array 1
-      # shovels elements 6-8 into sector array 2
+      # shovels elements 0-2 into sectors[0]
+      # shovels elements 3-5 into sectors[1]
+      # shovels elements 6-8 into sectors[2]
     # for row arrays 3-5
-      # shovels elements 0-2 into sector array 3
-      # shovels elements 3-5 into sector array 4
-      # shovels elements 6-8 into sector array 5
+      # shovels elements 0-2 into sectors[3]
+      # shovels elements 3-5 into sectors[4]
+      # shovels elements 6-8 into sectors[5]
     # for row arrays 6-8
-      # shovels elements 0-2 into sector array 6
-      # shovels elements 3-5 into sector array 7
-      # shovels elements 6-8 into sector array 8
-
+      # shovels elements 0-2 into sectors[6]
+      # shovels elements 3-5 into sectors[7]
+      # shovels elements 6-8 into sectors[8]
+    count = 0
     sectors = Array.new(9) {Array.new}
-    @grid.each do |row|
-      sector.each_with_index
+    @grid.each_slice(3) do |group| # group is an array of 3 "row" arrays
+      group.each do |row|
+        sectors[count] << row[0..2].flatten
+        # require 'pry'; binding.pry
+        sectors[count+1] << row[3..5]
+        sectors[count+2] << row[6..8]
+      end
+      count += 3
     end
+    sectors.map(&:flatten)
   end
 
   def valid_sectors?
     # check that integers 1..9 appear in each 3x3 section exactly once
-
+    create_sectors.all? do |sector|
+      sector.valid_numbers?
+    end
   end
 
   def valid_placement?
     # if all 3 dimensions are valid placements, true
-    # horizontal validity is ensured upon row generation
-    if valid_columns? && valid_sectors?
-      true
-    end
+    # (horizontal validity is ensured upon row generation)
+    true if valid_columns? && valid_sectors?
   end
 
   # def number_of_givens
@@ -134,4 +144,4 @@ end
 board = Board.new
 # board.example_grid
 # board.print_grid
-p board.create_columns
+p board.create_sectors
